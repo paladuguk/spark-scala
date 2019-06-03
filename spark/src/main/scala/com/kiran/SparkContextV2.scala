52ed505ecd6265c0a89089a076a4b7582047154c
paladuguk <kirankpaladugu@gmail.com>
2019-06-03 00:19:57 -0700

    Code cleanup

--- spark/src/main/scala/com/kiran/SparkContextV2.scala
+++ spark/src/main/scala/com/kiran/SparkContextV2.scala
@@ -6,36 +6,18 @@ object SparkContextV2 {
 
 	def main(args: Array[String]) : Unit = {
 
-			val session = SparkSession
+		val session = SparkSession
 					.builder()
 					.appName("Spark Context V2")
 					.master("local")
 					.getOrCreate()
 			
-			val rdd = session.sparkContext.textFile("/Users/kiran/Downloads/FL_insurance_sample.csv", 2)
+		val rdd = session.sparkContext.textFile("/Users/kiran/Downloads/FL_insurance_sample.csv", 2)
 			
-			val header = rdd.first
+		val header = rdd.first
 			
-			val csvRdd = rdd.take(10).filter(_ != header)
+		val csvRdd = rdd.take(10).filter(_ != header)
 			
-			csvRdd.foreach(println)
-			
-			val finalRDD = csvRdd.map(line => {
-			    val cols = line.split(",")
-			    print("cols ", cols(0))
-			    Array(cols(0), cols(1)).mkString(",")
-			})
-			
-			finalRDD.foreach(print)
-			
-		
-			
-			
-			
-			
-			
-			
-			
+		csvRdd.foreach(println)
 	}
-
 }
